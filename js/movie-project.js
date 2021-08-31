286e3bf552841f2b82b8121118ce97e87cb63124
Anthony Villegas <villegasanthony87@yahoo.com>
2021-08-31 01:03:22 -0500

    feat: added css file & directory

--- js/movie-project.js
+++ js/movie-project.js
@@ -12,9 +12,9 @@ $(document).ready(function(){
     const posterPromise = fetch(`https://api.themoviedb.org/3/search/movie?api_key=${movieKey}&query=IT`)
         posterPromise.then(result => {
             result.json().then(poster => {
-                console.log(poster)
+                console.log(poster);
             })
-        })
+        });
 
     // Movie API call
     const moviePromise = fetch(API_URL)
@@ -23,7 +23,7 @@ $(document).ready(function(){
                 movie.innerHTML = getMovies(movies);
 
             })
-        })
+        });
 
 
     //Render Movies function
@@ -32,26 +32,26 @@ $(document).ready(function(){
 
             console.log(movies);
         movies.forEach(movie => {
-            let moviePoster = movie.poster
+            //let moviePoster = movie.poster
             html += `<div class="card">
-              <div class="card-image waves-effect waves-block waves-light">
-                <img class="activator" src="${posterEndPoint+testImg}">
-              </div>
-              <div class="card-content">
-                <span class="card-title activator grey-text text-darken-4">${movie.title}<i class="material-icons right">more_vert</i></span>
-                <p>Rating: ${movie.rating}</p>
-                <p><a href="#">Hi!</a></p>
-              </div>
-              <div class="card-reveal">
-                <span class="card-title grey-text text-darken-4">Actors: ${movie.actors}<i class="material-icons right">close</i></span>
-                <p>Plot: ${movie.plot}</p>
-                <p>Year: ${movie.year}</p>
-              </div>
-            </div> `
+                  <div class="card-image waves-effect waves-block waves-light">
+                    <img class="activator" src="${posterEndPoint+testImg}">
+                  </div>
+                  <div class="card-content">
+                    <span class="card-title activator grey-text text-darken-4">${movie.title}<i class="material-icons right">more_vert</i></span>
+                    <p>Rating: ${movie.rating}</p>
+                    <p><a href="#">Hi!</a></p>
+                  </div>
+                  <div class="card-reveal">
+                    <span class="card-title grey-text text-darken-4">Actors: ${movie.actors}<i class="material-icons right">close</i></span>
+                    <p>Plot: ${movie.plot}</p>
+                    <p>Year: ${movie.year}</p>
+                  </div>
+                </div> `
         })
         return html
 
     }
 
-})
+});
 
